2252c3c77865dd1246021aa3b82ae2717a4ab6d2
Dimonch1k <137522769+Dimonch1k@users.noreply.github.com>
2026-08-30 19:28:42 +0300

    finish movies and shows page

--- src/components/sections/movies-and-shows/movies-and-shows-hero-section.tsx
+++ src/components/sections/movies-and-shows/movies-and-shows-hero-section.tsx
@@ -1,11 +1,44 @@
-import { Legend } from '@/components/ui/legend'
+'use client'
+
+import Image from 'next/image'
+
+import { useScreenType } from '@/hooks/useScreenType'
 
 export function MoviesAndShowsHeroSection() {
+	const screenType = useScreenType()
+
 	return (
 		<>
-			<Legend header='Movies and Shows'>
-				<div>Content</div>
-			</Legend>
+			{screenType === 'desktop' && (
+				<Image
+					src='/movies-and-shows/movies-and-shows-hero-desktop.png'
+					alt='Movies and Shows Hero'
+					width={1594}
+					height={835}
+					className='w-full h-full object-contain'
+					draggable={false}
+				/>
+			)}
+			{screenType === 'laptop' && (
+				<Image
+					src='/movies-and-shows/movies-and-shows-hero-laptop.png'
+					alt='Movies and Shows Hero'
+					width={1280}
+					height={709}
+					className='w-full h-full object-contain'
+					draggable={false}
+				/>
+			)}
+			{screenType === 'mobile' && (
+				<Image
+					src='/movies-and-shows/movies-and-shows-hero-mobile.png'
+					alt='Movies and Shows Hero'
+					width={358}
+					height={468}
+					className='w-full h-full object-contain'
+					draggable={false}
+				/>
+			)}
 		</>
 	)
 }
